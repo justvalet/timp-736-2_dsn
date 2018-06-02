87d1363f1c40eb649381fbac83406b3ad1c88413
Даржаев <736-2_dsn@timp2018.novalocal>
2018-06-02 12:37:25 +0000

    commit1

---
+++ 736-2_dsn-1-2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () 
+{
+int a,b,c;
+	printf("Vedite chisla dly slojenia - ");		
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("Summa ravna = %d " , c);
+	getchar(); getchar();
+return 0;
+}
